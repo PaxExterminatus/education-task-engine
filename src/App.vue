0f8e7332f1ebe1c57c3021294e7001ea290999e3
Paxexterminatus <Paxexterminatus@gmail.com>
2018-02-16 04:57:30 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  #app.edu-task-app
+    edu-task-set
+</template>
+
+<script>
+import eduTaskSet from './components/eduTask/eduTaskSet'
+
+export default {
+  name: 'App',
+  components: {
+    eduTaskSet
+  }
+}
+</script>
+
+<style lang="sass">
+.edu-task-app
+  border: 1px solid red
+  height: 94vh
+  max-height: 480px
+</style>
---
+++ src/components/eduTask/eduTaskSet.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  div.edu-tasks-set
+    div.totals {{msg}}
+</template>
+
+<script>
+export default {
+  name: 'edu-task-set',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped lang="sass">
+.edu-tasks-set
+  .totals
+    text-align: center
+</style>
