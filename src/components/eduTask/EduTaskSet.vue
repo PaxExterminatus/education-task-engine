400ca93b90293265e09dd0d8e98a9c77363c0741
Paxexterminatus <Paxexterminatus@gmail.com>
2018-03-04 18:22:05 +0300

    ref all

--- src/components/eduTask/EduTaskSet.vue
+++ src/components/eduTask/EduTaskSet.vue
@@ -1,25 +1,24 @@
 <template lang="pug">
-.edu-app-set
-  div(v-if="taskShow")
-    .edu-app-note
-      .edu-app-set-counter Задание {{index + 1}} из {{tasks.length}}
+  .edu-app-set {{changes}}
+    .edu-app-task(v-if="taskShown")
+      .edu-app-task-counter Задание {{index + 1}} из {{tasks.length}}
       .edu-app-task-todo {{task.todo}}
       .edu-app-task-question {{task.question.label}}
-    .edu-app-actions
-      edu-task(v-bind:task="task" v-on:answerChangeEvent="answerChangeEvent")
-      button.edu-app-action(v-if="actionShowNext" v-bind:disabled="answerNotChanged" v-on:click="nextTask") Дальше
-      button.edu-app-action(v-if="actionShowCheck" v-bind:disabled="answerNotChanged" v-on:click="resultCheck") Проверить
-  div.edu-app-resume(v-if="actionsShowResume") В последний раз вы не закончили все задания. Продолжите выполнение с {{cookieIndex + 1}} задания или начните c начала
-    button.edu-app-action(v-on:click="tasksResume") Продолжить
-    button.edu-app-action(v-on:click="tasksStart") С начала
-  .edu-app-result(v-if="showResult")
-    .edu-app-result-counter Вы правильно ответили на
-      span.edu-app-result-percent {{resultPercent}}%
-      | вопросов
-      div ({{resultCorrect}} из {{tasks.length}})
-    .edu-app-result-message(v-html="resultMessage")
-    div.edu-app-resume
-      button.edu-app-action(v-on:click="tasksStart") Еще раз
+      .edu-app-actions
+        edu-task(v-bind:task="task" v-on:answerChangeEvent="answerChangeEvent")
+        button.edu-app-action(v-if="actionShowNext" v-bind:disabled="!answerChanged" v-on:click="nextTask") Дальше
+        button.edu-app-action(v-if="actionShowCheck" v-bind:disabled="!answerChanged" v-on:click="resultCheck") Проверить
+    .edu-app-resume(v-if="resumeShown") В последний раз вы не закончили все задания. Продолжите выполнение с {{indexCookie + 1}} задания или начните c начала
+      .edu-app-actions
+        button.edu-app-action(v-on:click="setResume") Продолжить
+        button.edu-app-action(v-on:click="setRestart") С начала
+    .edu-app-result(v-if="resultShown")
+      .edu-app-result-counter Вы правильно ответили на
+        span.edu-app-result-percent {{resultPercent}}%
+        | вопросов ({{resultCorrect}} из {{tasks.length}})
+        .edu-app-result-message(v-html="resultMessage")
+      .edu-app-actions
+        button.edu-app-action(v-on:click="setRestart") Еще раз
 </template>
 
 <script>
@@ -43,111 +42,60 @@ export default {
       },
       resultCorrect: 0,
       resultMessage: '',
-      showResult: false,
-      answerNotChanged: true,
-      actionsResumeChange: false
+      answerChanged: false,
+      // Show block
+      taskShow: true,
+      resumeShow: false,
+      resultShow: false
     }
   },
-  beforeCreate () {
-    this.id = document.getElementById('task-set').getAttribute('data-id')
+  mounted () {
+    this.id = Number(document.getElementById('task-set').getAttribute('data-id'))
     const url = 'http://localhost/set/' + this.id + '.json' // dev
     // 'https://www.eshko.by/source_lib/data/task_set/' + this.id + '.json' // product
     http.get(url)
       .then(response => {
-        this.id = response.data['id']
         this.messages = response.data['messages']
         this.tasks = response.data['tasks']
       })
-    console.log(this.showResult)
-  },
-  watch: {
-    index: function (val) {
-      cookies.set('edu-tasks-' + this.id + '-index', val)
-      cookies.set('edu-tasks-' + this.id + '-changes', JSON.stringify(this.changes))
-    },
-    showResult: function (val) {
-      cookies.set('edu-tasks-' + this.id + '-result', val)
+    this.resultShow = this.resultCookie
+    if (this.resultShow) {
+      this.changes = this.changesCookie
     }
-  },
-  methods: {
-    tasksResume: function () {
-      this.index = this.cookieIndex
-      this.changes = this.cookieChanges
-      this.actionsResumeChange = true
-    },
-    tasksStart: function () {
-      cookies.delete('edu-tasks-' + this.id + '-index')
-      cookies.delete('edu-tasks-' + this.id + '-changes')
-      cookies.delete('edu-tasks-' + this.id + '-result')
-      this.index = 0
-      this.showResult = false
-      this.actionsResumeChange = true
-    },
-    answerChangeEvent: function (val) {
-      if (val) {
-        this.changes.answers[this.index] = val
-        this.answerNotChanged = false
-      }
-    },
-    nextTask: function () {
-      this.index += 1
-      this.answerNotChanged = true
-    },
-    resultCheck: function () {
-      this.resultCorrect = 0
-      cookies.set('edu-tasks-' + this.id + '-changes', JSON.stringify(this.changes))
-      for (let i = 0; i < this.tasks.length; i++) {
-        if (this.changes.answers[i] > 0) {
-          if (this.tasks[i].correct === this.changes.answers[i]) {
-            this.resultCorrect += 1
-          }
-        }
-      }
-      this.resultMessage = ''
-      for (let i = 0; i < this.messages.length; i++) {
-        if (this.resultPercent >= this.messages[i].percent) {
-          this.resultMessage = this.messages[i].message
-        }
-      }
-      this.showResult = true
-      cookies.set('edu-tasks-' + this.id + '-result', true)
+    if (this.indexCookie > 0) {
+      this.resumeShow = true
     }
   },
   computed: {
-    task: function () {
+    taskShown () {
+      return this.taskShow
+    },
+    resumeShown () {
+      return this.resumeShow
+    },
+    resultShown () {
+      return this.resultShow
+    },
+    resultCookie () {
+      return cookies.get(this.resultCookieName) === 'true'
+    },
+    indexCookie () {
+      return Number(cookies.get(this.indexCookieName))
+    },
+    changesCookie () {
+      return JSON.parse(cookies.get(this.changesCookieName))
+    },
+    resultCookieName () {
+      return 'edu-tasks-' + this.id + '-result'
+    },
+    indexCookieName () {
+      return 'edu-tasks-' + this.id + '-index'
+    },
+    changesCookieName () {
+      return 'edu-tasks-' + this.id + '-changes'
+    },
+    task () {
       return this.tasks[this.index]
-    },
-    lastTask: function () {
-      return this.tasks.length === this.index + 1
-    },
-    resultPercent: function () {
-      return Number((this.resultCorrect / this.tasks.length * 100).toFixed(0))
-    },
-    /**
-     * Last task index in cookies
-     * @returns {number}
-     */
-    cookieIndex: function () {
-      return Number(cookies.get('edu-tasks-' + this.id + '-index'))
-    },
-    /**
-     * Task Set changes in cookies
-     * @returns {string}
-     */
-    cookieChanges: function () {
-      return JSON.parse(cookies.get('edu-tasks-' + this.id + '-changes'))
-    },
-    cookieResult: function () {
-      return cookies.get('edu-tasks-' + this.id + '-result') === 'true'
-    },
-    resultShow: function () {
-      if (this.cookieResult) {
-        this.tasksResume()
-        this.resultCheck()
-        return true
-      }
-      if (this.showResult) return true
-      return false
     },
     actionShowNext: function () {
       return !this.lastTask
@@ -155,17 +103,82 @@ export default {
     actionShowCheck: function () {
       return this.lastTask
     },
-    actionsShowResume: function () {
-      return (this.cookieIndex > 0 && !this.actionsResumeChange)
+    lastTask: function () {
+      return this.tasks.length === this.index + 1
     },
-    taskShow: function () {
-      if (this.actionsShowResume) {
-        return false
-      } else if (this.showResult) {
-        return false
-      } else {
-        return true
+    resultPercent: function () {
+      return Number((this.resultCorrect / this.tasks.length * 100).toFixed(0))
+    }
+  },
+  watch: {
+    tasks (val) {
+      if (this.resultShow) {
+        this.resultCheck()
       }
+    },
+    taskShow (val) {
+      if (val) {
+        this.resumeShow = false
+        this.resultShow = false
+      }
+    },
+    resultShow (val) {
+      if (val) {
+        this.taskShow = false
+        this.resumeShow = false
+      }
+      cookies.set(this.resultCookieName, val)
+    },
+    resumeShow (val) {
+      if (val) {
+        this.taskShow = false
+        this.resultShow = false
+      }
+    },
+    index: function (val) {
+      cookies.set(this.indexCookieName, val)
+      cookies.set(this.changesCookieName, JSON.stringify(this.changes))
+    }
+  },
+  methods: {
+    answerChangeEvent: function (val) {
+      if (val) {
+        this.changes.answers[this.index] = val
+        this.answerChanged = true
+      }
+    },
+    nextTask: function () {
+      this.index += 1
+      this.answerChanged = false
+    },
+    resultCheck: function () {
+      this.resultCorrect = 0
+      for (let i = 0; i < this.tasks.length; i++) {
+        if (this.changes.answers[i] > 0) {
+          if (this.tasks[i].correct === this.changes.answers[i]) {
+            this.resultCorrect += 1
+          }
+        }
+      }
+      for (let i = 0; i < this.messages.length; i++) {
+        if (this.resultPercent >= this.messages[i].percent) {
+          this.resultMessage = this.messages[i].message
+        }
+      }
+      this.resultShow = true
+    },
+    setRestart () {
+      cookies.delete(this.indexCookieName)
+      cookies.delete(this.changesCookieName)
+      cookies.delete(this.resultCookieName)
+      this.index = 0
+      this.changes = {answers: [0]}
+      this.taskShow = true
+    },
+    setResume: function () {
+      this.index = this.indexCookie
+      this.changes = this.changesCookie
+      this.taskShow = true
     }
   }
 }
